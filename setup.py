049933a29d3526893db0a10aadb82258d24b3c28
Thao Le <thaole.tau@gmail.com>
2016-12-27 17:38:57 +0000

    Added more necessary packages for install

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version = "0.1.0",
     packages = find_packages(exclude=['*test']),
     scripts = ['scripts/graph'],
-    install_requires = ['argparse'],
+    install_requires = ['argparse','numpy','geopy','matplotlib'],
     description = "Generates a rough graph of the amount of greenery between two locations",
     license = "MIT"
 )

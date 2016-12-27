0a0bea4875abe556ef5fdf3dcb0ce98f661dfe98
Thao Le <thaole.tau@gmail.com>
2016-12-27 16:56:24 +0000

    Added correct syntax/punctuation to setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version = "0.1.0",
     packages = find_packages(exclude=['*test']),
     scripts = ['scripts/graph'],
-    install_requires = ['argparse']
-	description= "Generates a rough graph of the amount of greenery between two locations"
-	license = "MIT"
+    install_requires = ['argparse'],
+    description = "Generates a rough graph of the amount of greenery between two locations",
+    license = "MIT"
 )

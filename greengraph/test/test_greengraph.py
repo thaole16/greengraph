0a3f8900bb1e8d286ece91e1a7627942d58619c4
Thao Le <thaole.tau@gmail.com>
2017-01-01 10:40:12 +0000

    Added a basic testing function for greengraph geolocate

--- greengraph/test/test_greengraph.py
+++ greengraph/test/test_greengraph.py
@@ -1,10 +1,22 @@
 import pytest
+import os
+from greengraph.greengraph import Greengraph
+
+#https://www.python.org/dev/peps/pep-0485/#proposed-implementation
+def isclose(a, b, rel_tol=1e-09, abs_tol=0.0):
+    return abs(a-b) <= max(rel_tol * max(abs(a), abs(b)), abs_tol)
 
 def test_Greengraph_init():
-    from greengraph.greengraph import Greengraph
     mygraph = Greengraph('New York','Chicago')
     assert mygraph.start == 'New York'
     assert mygraph.end == 'Chicago'
     assert mygraph.end != 'Dog'
     assert mygraph.geocoder
 
+def test_geolocate():
+    mygraph = Greengraph('New York', 'Chicago')
+    coords = mygraph.geolocate('New York')
+    assert coords
+    assert type(coords) == tuple
+    assert isclose(coords[0],40.7127837)
+    assert isclose(coords[1], -74.0059413)

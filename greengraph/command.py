651a59479990727c42ec6a9582ca8e395004288e
Thao Le <thaole.tau@gmail.com>
2017-01-01 14:34:38 +0000

    Output figure from command.py is now saved into a png file

--- greengraph/command.py
+++ greengraph/command.py
@@ -4,20 +4,24 @@ from matplotlib import pyplot as plt
 
 def process():
     parser = ArgumentParser(description = "Generates a graph of the porportion of green pixels between two locations")
- 
-    parser.add_argument('start', help="Start Location")
-    parser.add_argument('end', help = "End Location")
-    parser.add_argument('steps', help="Steps inbetween")
+    #use "New York" (etc) for a longer name
+    parser.add_argument('--start', help="Start Location")
+    parser.add_argument('--end', help="End Location")
+    parser.add_argument('--steps', help="Steps inbetween")
+    parser.add_argument('--imageout', help="File name for output figure")
+
 
     arguments= parser.parse_args()
 
-    mygraph = Greengraph(arguments.start,arguments.end)
+    mygraph = Greengraph(arguments.start, arguments.end)
     data = mygraph.green_between(arguments.steps)
     print(data)
-    plt.figure("Number of green pixels between " + arguments.start +" and " + arguments.end)
-
+    plt.title("Number of green pixels between " + arguments.start +" and " + arguments.end)
+    plt.ylabel('Number of green pixels')
+    plt.xlabel('Steps in-between')
     plt.plot(data)
-    plt.show()
+    #plt.show()
+    plt.savefig(arguments.imageout)
    
 if __name__ == "__main__":
     process()

e02b3600515104f657ce44f93850b14d00b17077
Thao Le <thaole.tau@gmail.com>
2017-01-01 14:11:59 +0000

    Changed the output of command.py (plot now appears, and the data is printed)

--- greengraph/command.py
+++ greengraph/command.py
@@ -1,18 +1,23 @@
 from argparse import ArgumentParser
-from .greengraph import Greengraph, Map
+from greengraph import Greengraph, Map
+from matplotlib import pyplot as plt
 
 def process():
     parser = ArgumentParser(description = "Generates a graph of the porportion of green pixels between two locations")
  
-    parser.add_argument('start')
-    parser.add_argument('end')
-    parser.add_argument('steps')
+    parser.add_argument('start', help="Start Location")
+    parser.add_argument('end', help = "End Location")
+    parser.add_argument('steps', help="Steps inbetween")
 
     arguments= parser.parse_args()
 
     mygraph = Greengraph(arguments.start,arguments.end)
-	data = mygraph.green_between(arguments.steps)
-	plt.plot(data)
+    data = mygraph.green_between(arguments.steps)
+    print(data)
+    plt.figure("Number of green pixels between " + arguments.start +" and " + arguments.end)
+
+    plt.plot(data)
+    plt.show()
    
 if __name__ == "__main__":
     process()

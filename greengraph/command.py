66c62f742ddd64b55b0ff0a40396ad0e61ad5b69
Thao Le <thaole.tau@gmail.com>
2017-01-01 14:55:34 +0000

    Data now printed out to file (CSV)

--- greengraph/command.py
+++ greengraph/command.py
@@ -1,6 +1,7 @@
 from argparse import ArgumentParser
 from greengraph import Greengraph, Map
 from matplotlib import pyplot as plt
+import csv
 
 def process():
     parser = ArgumentParser(description = "Generates a graph of the porportion of green pixels between two locations")
@@ -8,20 +9,26 @@ def process():
     parser.add_argument('--start', help="Start Location")
     parser.add_argument('--end', help="End Location")
     parser.add_argument('--steps', help="Steps inbetween")
-    parser.add_argument('--imageout', help="File name for output figure")
-
+    parser.add_argument('--imageout', help="File name for output figure (PNG)")
+    parser.add_argument('--dataout', help="File name for data output (list of numbers of green pixels)")
 
     arguments= parser.parse_args()
 
     mygraph = Greengraph(arguments.start, arguments.end)
     data = mygraph.green_between(arguments.steps)
-    print(data)
+
+    with open(arguments.dataout, "w") as outfile:
+        writer = csv.writer(outfile, lineterminator='\n')
+        for num in data:
+            writer.writerow([num])
+
     plt.title("Number of green pixels between " + arguments.start +" and " + arguments.end)
     plt.ylabel('Number of green pixels')
     plt.xlabel('Steps in-between')
     plt.plot(data)
     #plt.show()
     plt.savefig(arguments.imageout)
-   
+
+
 if __name__ == "__main__":
     process()

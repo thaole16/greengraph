ae469a8ff84ec11fb92a4142145ffa8c86c10b73
Thao Le <thaole.tau@gmail.com>
2017-01-01 11:19:54 +0000

    Added basic function for testing Map show_green

--- greengraph/test/test_map.py
+++ greengraph/test/test_map.py
@@ -19,5 +19,10 @@ def test_green():
     assert (greenar==expected).all()
 
 def test_count_green():
-    newyork = Map(40.7127837, -74.0059413, size=(2, 2));
+    newyork = Map(40.7127837, -74.0059413, size=(2, 2))
     assert newyork.count_green()==0
+
+def test_show_green():
+    newyork = Map(40.7127837, -74.0059413, size=(2, 2))
+    expected = b'\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR\x00\x00\x00\x02\x00\x00\x00\x02\x08\x06\x00\x00\x00r\xb6\r$\x00\x00\x00\x04sBIT\x08\x08\x08\x08|\x08d\x88\x00\x00\x00\tpHYs\x00\x00\x0fa\x00\x00\x0fa\x01\xa8?\xa7i\x00\x00\x00\x14IDAT\x08\x99cd``\xf8\xcf\xc0\xc0\xc0\xc0\xc4\x00\x05\x00\x0e(\x01\x03\xb68\xca\xd3\x00\x00\x00\x00IEND\xaeB`\x82'
+    assert newyork.show_green()==expected

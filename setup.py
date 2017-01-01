34dad920fcfe7f1b51a413f30aaf29513b791f26
Thao Le <thaole.tau@gmail.com>
2017-01-01 15:17:36 +0000

    Added command.py to console scripts; greengraph now works via commandline

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version = "0.1.0",
     packages = find_packages(exclude=['*test']),
     scripts = ['scripts/graph'],
+    entry_points = {'console_scripts': ['greengraph=greengraph.command:process']},
     install_requires = ['argparse','numpy','geopy','matplotlib'],
     description = "Generates a rough graph of the amount of greenery between two locations",
     license = "MIT"

636c368dd88531996596209ab14f9074e19ebca3
Thao Le <thaole.tau@gmail.com>
2017-01-02 17:00:51 +0000

    Mocked up geopy.geocoder in test_greengraph

--- greengraph/test/test_greengraph.py
+++ greengraph/test/test_greengraph.py
@@ -1,10 +1,13 @@
 import pytest
-import os
 from greengraph.greengraph import Greengraph
+from mock import patch, Mock
+import geopy
 
-#https://www.python.org/dev/peps/pep-0485/#proposed-implementation
+
+# https://www.python.org/dev/peps/pep-0485/#proposed-implementation
 def isclose(a, b, rel_tol=1e-09, abs_tol=0.0):
-    return abs(a-b) <= max(rel_tol * max(abs(a), abs(b)), abs_tol)
+    return abs(a - b) <= max(rel_tol * max(abs(a), abs(b)), abs_tol)
+
 
 @pytest.fixture(scope="module")
 def graphobj():
@@ -17,20 +20,28 @@ def test_Greengraph_init(graphobj):
     assert graphobj.geocoder
 
 def test_geolocate(graphobj):
-    coords = graphobj.geolocate('New York')
-    assert coords
-    assert type(coords) == tuple
-    assert isclose(coords[0],40.7127837)
-    assert isclose(coords[1], -74.0059413)
+    with patch.object(graphobj.geocoder, 'geocode') as mock_get:
+        mock_get.return_value = [geopy.location.Location('New York, NY, USA', (40.7127837, -74.0059413, 0.0))]
+        coords = graphobj.geolocate('New York')
+        mock_get.assert_called_with(
+            'New York',
+            exactly_one=False
+        )
+        assert coords
+        assert type(coords) == tuple
+        assert isclose(coords[0], 40.7127837)
+        assert isclose(coords[1], -74.0059413)
+
 
 def test_location_sequence(graphobj):
-    sequence=graphobj.location_sequence((0, 0), (1, 1), 2)
-    expected = [[ 0.,  0.],[ 1.,  1.]]
-    assert (sequence==expected).all()
+    sequence = graphobj.location_sequence((0, 0), (1, 1), 2)
+    expected = [[0., 0.], [1., 1.]]
+    assert (sequence == expected).all()
 
     sequence = graphobj.location_sequence((0, 0), (0, 1), 2)
     expected = [[0., 0.], [0., 1.]]
     assert (sequence == expected).all()
+
 
 def test_green_between(graphobj):
     datapts = graphobj.green_between(2)

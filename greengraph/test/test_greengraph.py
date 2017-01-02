728087dae45908bb25d7caec23205605c4dd67cd
Thao Le <thaole.tau@gmail.com>
2017-01-02 19:27:46 +0000

    Added more mocks to test_green_between function; now it doesn't have any internet calls

--- greengraph/test/test_greengraph.py
+++ greengraph/test/test_greengraph.py
@@ -46,7 +46,13 @@ def test_location_sequence(graphobj):
 def test_green_between(graphobj):
     with patch.object(graphobj,'location_sequence') as MockClass: #mock a function call
         MockClass.return_value =[[0., 0.], [1., 1.]]
-        with patch.object(Map,'count_green') as MockMap: #mock the Map (API hidden) call
-            MockMap.return_value = 0
-            datapts = graphobj.green_between(2)
-            assert datapts==[0,0]
+        with patch.object(graphobj.geocoder, 'geocode') as mock_get:
+            mock_get.return_value = [geopy.location.Location('New York, NY, USA', (40.7127837, -74.0059413, 0.0))]
+            with patch.object(requests, 'get') as mock_get:
+                expectedimage = b'\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR\x00\x00\x00\x02\x00\x00\x00\x02\x04\x03\x00\x00\x00\x80\x98\x10\x17\x00\x00\x00\x0fPLTETPD``Td`Thh\\\xff\xff\xff\x13\xd3\x10z\x00\x00\x00\x01bKGD\x04\x8fh\xd9Q\x00\x00\x00\x0cIDAT\x08\xd7c0`\x10\x02\x00\x00\xa6\x00C\xb6\xe8\xbe\xee\x00\x00\x00\x00IEND\xaeB`\x82'
+                mock_get.return_value = mock_response = Mock()
+                mock_response.content = expectedimage
+                with patch.object(Map,'count_green') as MockMap: #mock the Map (API hidden) call
+                    MockMap.return_value = 0
+                    datapts = graphobj.green_between(2)
+                    assert datapts==[0,0]

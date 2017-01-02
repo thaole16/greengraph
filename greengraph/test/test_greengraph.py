fa8c1d4d44cba6f16d6e3d2f973add6d359c9c74
Thao Le <thaole.tau@gmail.com>
2017-01-02 12:55:59 +0000

    Added a fixture to test_greengraph for the greengraph object

--- greengraph/test/test_greengraph.py
+++ greengraph/test/test_greengraph.py
@@ -6,33 +6,33 @@ from greengraph.greengraph import Greengraph
 def isclose(a, b, rel_tol=1e-09, abs_tol=0.0):
     return abs(a-b) <= max(rel_tol * max(abs(a), abs(b)), abs_tol)
 
-def test_Greengraph_init():
-    mygraph = Greengraph('New York','Chicago')
-    assert mygraph.start == 'New York'
-    assert mygraph.end == 'Chicago'
-    assert mygraph.end != 'Dog'
-    assert mygraph.geocoder
+@pytest.fixture(scope="module")
+def graphobj():
+    return Greengraph('New York', 'Chicago')
 
-def test_geolocate():
-    mygraph = Greengraph('New York', 'Chicago')
-    coords = mygraph.geolocate('New York')
+def test_Greengraph_init(graphobj):
+    assert graphobj.start == 'New York'
+    assert graphobj.end == 'Chicago'
+    assert graphobj.end != 'Dog'
+    assert graphobj.geocoder
+
+def test_geolocate(graphobj):
+    coords = graphobj.geolocate('New York')
     assert coords
     assert type(coords) == tuple
     assert isclose(coords[0],40.7127837)
     assert isclose(coords[1], -74.0059413)
 
-def test_location_sequence():
-    mygraph = Greengraph('New York', 'Chicago')
-    sequence=mygraph.location_sequence((0, 0), (1, 1), 2)
+def test_location_sequence(graphobj):
+    sequence=graphobj.location_sequence((0, 0), (1, 1), 2)
     expected = [[ 0.,  0.],[ 1.,  1.]]
     assert (sequence==expected).all()
 
-    sequence = mygraph.location_sequence((0, 0), (0, 1), 2)
+    sequence = graphobj.location_sequence((0, 0), (0, 1), 2)
     expected = [[0., 0.], [0., 1.]]
     assert (sequence == expected).all()
 
-def test_green_between():
-    mygraph = Greengraph('New York', 'Chicago')
-    datapts = mygraph.green_between(2)
+def test_green_between(graphobj):
+    datapts = graphobj.green_between(2)
     expected = [60300, 28125]
     assert datapts == expected

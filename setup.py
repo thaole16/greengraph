e0021c1c17f75b87b49b55794da0317b520074bf
Thao Le <thaole.tau@gmail.com>
2017-01-04 09:42:55 +0000

    Setup.py: formatted, added details (classifiers, url, author, install requires)

--- setup.py
+++ setup.py
@@ -1,12 +1,30 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "Greengraph",
-    version = "0.1.0",
-    packages = find_packages(exclude=['*test']),
-    scripts = ['scripts/graph'],
-    entry_points = {'console_scripts': ['greengraph=greengraph.command:process']},
-    install_requires = ['argparse','numpy','geopy','matplotlib'],
-    description = "Generates a rough graph of the amount of greenery between two locations",
-    license = "MIT"
+    name="Greengraph",
+
+    version="0.1.1",
+
+    description="Generates a rough graph of the amount of greenery between two locations",
+
+    url='https://github.com/UCL/rsd-engineeringcourse',
+
+    author='MPHYG001, Thao Le',
+
+    packages=find_packages(exclude=['*test','docs']),
+
+    scripts=['scripts/graph'],
+
+    entry_points={'console_scripts': ['greengraph=greengraph.command:process']},
+
+    install_requires=['argparse','numpy','geopy','matplotlib','requests','csv'],
+
+    license="MIT",
+
+    classifiers=[
+        'License :: MIT License',
+        'Intended Audience :: MPHYG001 Markers',
+        'Programming Language :: Python :: 2.7',
+
+    ]
 )

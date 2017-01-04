92584f81a142bf911468d24985f4a8f0e12c9e9c
Thao Le <thaole.tau@gmail.com>
2017-01-04 12:42:48 +0000

    Tested the expected pixels as 'Did it return the right size' instead (is more meaningful than testing against a fixture)

--- greengraph/test/test_map.py
+++ greengraph/test/test_map.py
@@ -32,10 +32,8 @@ def newyork():
 
 def test_Map_init(newyork):
     expectedimage = b'\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR\x00\x00\x00\x02\x00\x00\x00\x02\x04\x03\x00\x00\x00\x80\x98\x10\x17\x00\x00\x00\x0fPLTETPD``Td`Thh\\\xff\xff\xff\x13\xd3\x10z\x00\x00\x00\x01bKGD\x04\x8fh\xd9Q\x00\x00\x00\x0cIDAT\x08\xd7c0`\x10\x02\x00\x00\xa6\x00C\xb6\xe8\xbe\xee\x00\x00\x00\x00IEND\xaeB`\x82'
-    expectedpixels = [[[0.40784314, 0.40784314, 0.36078432], [0.32941177, 0.3137255, 0.26666668]],
-                      [[0.3764706, 0.3764706, 0.32941177], [0.39215687, 0.3764706, 0.32941177]]]
     assert newyork.image == expectedimage
-    assert np.allclose(newyork.pixels, expectedpixels)
+    assert np.shape(np.array(newyork.pixels))==(2,2,3)
 
 
 def test_green(newyork):

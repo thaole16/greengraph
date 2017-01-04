b0ff8c29dca17a867de692958cb597de65927e36
Thao Le <thaole.tau@gmail.com>
2017-01-04 21:20:08 +0000

    More fixtures conversions in test_map

--- greengraph/test/test_map.py
+++ greengraph/test/test_map.py
@@ -22,8 +22,6 @@ def newyork():
         mock_get.return_value = mock_response = Mock()
         mock_response.content = expectedimage
         default_map = Map(newyork['lat'],newyork['long'],size=(newyork['sizesquare'],newyork['sizesquare']))
-        #call = mock_get.mock_calls
-        #assert "http://maps.googleapis.com/maps/api/staticmap?" in call
         mock_get.assert_called_with(
             "http://maps.googleapis.com/maps/api/staticmap?",
             params={
@@ -40,22 +38,25 @@ def newyork():
 def test_Map_init(newyork):
     assert np.shape(np.array(newyork.pixels))==(2,2,3)
 
-
 def test_green(newyork):
+    placesfile = yaml.load(open(os.path.join(os.path.dirname(__file__), 'fixtures', 'places.yaml')))
+    newyorkexpected = placesfile[0]
     greenar = newyork.green(1.1)
-    expected = [[False, False], [False, False]]
+    expected = newyorkexpected['green']
     assert (greenar == expected).all()
 
 
 def test_count_green(newyork):
     with patch.object(newyork,'green') as MockClass:
-        MockClass.return_value =[[False, False], [False, False]]
-        assert newyork.count_green() == 0
+        placesfile = yaml.load(open(os.path.join(os.path.dirname(__file__), 'fixtures', 'places.yaml')))
+        newyorkexpected = placesfile[0]
+        MockClass.return_value = newyorkexpected['green']
+        assert newyork.count_green() == newyorkexpected['countgreen']
 
         MockClass.return_value = [[False, True], [False, True]]
         assert newyork.count_green() == 2
 
 
 def test_show_green(newyork):
-    expected = b'\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR\x00\x00\x00\x02\x00\x00\x00\x02\x08\x06\x00\x00\x00r\xb6\r$\x00\x00\x00\x04sBIT\x08\x08\x08\x08|\x08d\x88\x00\x00\x00\tpHYs\x00\x00\x0fa\x00\x00\x0fa\x01\xa8?\xa7i\x00\x00\x00\x14IDAT\x08\x99cd``\xf8\xcf\xc0\xc0\xc0\xc0\xc4\x00\x05\x00\x0e(\x01\x03\xb68\xca\xd3\x00\x00\x00\x00IEND\xaeB`\x82'
-    assert newyork.show_green() == expected
+    expectedgreen = file(os.path.join(os.path.dirname(__file__),'fixtures','newyork2x2green.png'),'rb').read()
+    assert newyork.show_green() == expectedgreen
